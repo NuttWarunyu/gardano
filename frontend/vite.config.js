0fde35067954e906ba05d44d586dfc7143452836
NuttWarunyu <NuttWarunyu@gmail.com>
2025-03-10 13:01:58 +0700

    Fix dependencies and update React version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-  preview: {
-    allowedHosts: ["gardano-production.up.railway.app"], // ✅ ใช้ URL ของ Railway หรือ Netlify
-  },
   define: {
     "import.meta.env.VITE_API_BASE_URL": JSON.stringify(
-      "https://gardano-production.up.railway.app/api" // ✅ ใช้ URL ของ Backend บน Railway
+      "https://gardano-production.up.railway.app/api" // URL ของ Backend ที่ Railway
     ),
   },
-  plugins: [react()], // ✅ ใช้ React Plugin ของ Vite
-  base: "/", // ✅ ตั้งค่าให้เป็น root path
+  plugins: [react()],
+  base: "/",
 });
