9c70b1b7b9792e667c98f369aadb9f38dcea029f
NuttWarunyu <NuttWarunyu@gmail.com>
2025-03-11 15:07:38 +0700

    Build and test frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react"; // นี่คือการติดตั้ง plugin React ของ Vite
 
 export default defineConfig({
-  root: ".", // กำหนด root path ให้เป็นโฟลเดอร์นี้ (ที่ไฟล์ public และ src อยู่)
+  root: ".", // กำหนด root path ให้เป็นโฟลเดอร์นี้
   publicDir: "public", // ระบุให้ Vite รู้ว่า public folder อยู่ที่ไหน
   build: {
     outDir: "dist", // ตั้งค่า output directory ให้เป็น dist
     rollupOptions: {
-      input: "index.html", // ระบุไฟล์ index.html ในโฟลเดอร์ frontend
+      input: "index.html", // ระบุไฟล์ index.html
     },
   },
   server: {
@@ -15,5 +15,5 @@ export default defineConfig({
     port: 4173, // ใช้ port ที่ไม่มีการใช้งานอื่น
   },
   plugins: [react()],
-  base: "/", // ตั้งค่าให้เป็น root path
+  base: "/",
 });
